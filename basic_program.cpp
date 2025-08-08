9446ca87548f5cd22fe2381fca473bc26b9bca81
mongooseintown <khaledbinnasir1714412140@gmail.com>
2025-08-08 22:27:08 +0600

    factorialfunction added to basic_program.cpp

--- basic_program.cpp
+++ basic_program.cpp
@@ -4,8 +4,19 @@ void helloworld()
 {
     cout<<"Hello World"<<endl;
 }
+long long factorial(int n)
+{
+    if (n==0 || n==1)
+    {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
 int main ()
 {
     helloworld();
+    int n;
+    cin>>n;
+    cout<<factorial(n)<<endl;
     return 0;
 }
